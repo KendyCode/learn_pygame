4dc6e965c8e9d23eb4960292e6f26d6a02069b83
KendyCode <kendyprox@gmail.com>
2026-02-23 18:20:36 +0100

    test surface colorée

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ pygame.display.set_caption("Runner")
 # Pour les FPS
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill("Red")
+
+# Pour que la fenetre reste ouverte
 while True:
+
     for event in pygame.event.get():
+        #Pour fermer la fenetre
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    # Pour mettre une surface sur une autre surface
+    screen.blit(test_surface,(200,100))
 
 
     pygame.display.update()
