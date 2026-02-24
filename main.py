67a3d99a2ef95fd61ccd072abf68a1bfd94a796d
KendyCode <kendyprox@gmail.com>
2026-02-24 15:39:17 +0100

    on a juste changer des couleurs sous rgb ou hex

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 #Antialisa en False puisque on va faire du pixel art sinon cest mieux de le mettre en True dans les autres cas
-score_surf = test_font.render("My game", False, "Black")
+score_surf = test_font.render("My game", False, (64,64,64))
 score_rect = score_surf.get_rect(center=(400,50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
@@ -39,10 +39,9 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    pygame.draw.rect(screen,"Pink", score_rect)
-    pygame.draw.rect(screen,"Pink", score_rect,10)
-    # Dessiner un cerlce
-    pygame.draw.ellipse(screen,"Brown",pygame.Rect(50,200,100,100))
+    pygame.draw.rect(screen,"#c0e8ec", score_rect)
+    pygame.draw.rect(screen,"#c0e8ec", score_rect,10)
+
     screen.blit(score_surf,score_rect)
 
     snail_rect.x -= 4
