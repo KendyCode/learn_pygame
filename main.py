c13a60336899ed44bc1b17a2b23e686f30ee58b8
KendyCode <kendyprox@gmail.com>
2026-02-24 11:56:24 +0100

    test surface texte

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 # Pour les FPS
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png")
 ground_surface = pygame.image.load("graphics/ground.png")
+#Antialisa en False puisque on va faire du pixel art sinon cest mieux de le mettre en True dans les autres cas
+text_surface = test_font.render("My game", False, "Black")
 # Pour que la fenetre reste ouverte
 while True:
 
@@ -21,7 +24,8 @@ while True:
     # Pour mettre une surface sur une autre surface
 
     screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,250))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
 
 
 
